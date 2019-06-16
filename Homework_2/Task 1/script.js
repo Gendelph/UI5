0ce68bb398bc53e329db9c8eeac5162204000970
Gendelph <sergey.ogur@gmail.com>
2019-06-16 13:00:33 +0300

    ht2

---
+++ Homework_2/Task 1/script.js
@@ -0,0 +1,19 @@
+function onClickB1() {
+    window.location.href = "http://www.google.com";
+}
+
+function onClickB2() {
+    document.body.innerHTML = '';
+}
+
+function CheckName() {
+    var name = prompt("Please enter name", "");
+    if (person == null || person == "") {
+        return;
+    }
+    if ( isNumber(person) ) {
+        
+    } else {
+        
+    }
+}
