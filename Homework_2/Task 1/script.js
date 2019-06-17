8f611517da45a2a21fefd447f8b2ed9fe25b6cf8
Gendelph <sergey.ogur@gmail.com>
2019-06-17 10:49:09 +0300

    Completed

--- Homework_2/Task 1/script.js
+++ Homework_2/Task 1/script.js
@@ -8,12 +8,34 @@ function onClickB2() {
 
 function CheckName() {
     var name = prompt("Please enter name", "");
-    if (person == null || person == "") {
+    if (name == null || name == "") {
+        alert('No Value');
         return;
     }
-    if ( isNumber(person) ) {
-        
+    if (hasNumber(name)) {
+        alert('Reversed Name: ' + reverseString(name));
     } else {
-        
+        alert('Your Name with mixed case: ' + mixCaseString(name));
     }
+}
+
+function hasNumber(str) {
+    return /\d/.test(str);
+}
+
+function reverseString(str){
+    return str.split("").reverse().join("");
+}
+
+function mixCaseString(str) {
+    var tmp = '';
+    for (var i = 0; i < str.length; ++i) {
+        let c = str[i];
+        if (i % 2 == 0) {
+            tmp = tmp + c.toUpperCase();
+        } else {
+            tmp = tmp + c.toLowerCase();
+        }
+    }
+    return tmp;
 }
